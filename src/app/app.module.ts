742a17eca522aea1bae4db9bd10facc272baf46b
JapxD <garciajapxd@gmail.com>
2022-09-21 23:27:31 +0800

    Initial design commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeTrackingDashboardComponent } from './time-tracking-dashboard/time-tracking-dashboard.component';
+import { TimeTrackingComponent } from './time-tracking-dashboard/time-tracking/time-tracking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeTrackingDashboardComponent,
+    TimeTrackingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/time-tracking-dashboard/time-tracking-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-time-tracking-dashboard',
+    templateUrl: 'time-tracking-dashboard.component.html',
+    styleUrls: ['time-tracking-dashboard.component.css']
+})
+
+export class TimeTrackingDashboardComponent{
+    
+}
